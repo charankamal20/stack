2ba537d96e5a15b7d9c88ae82536d41147c6cd2b
charankamal20 <charanksingh20@gmail.com>
2022-10-10 01:08:52 +0530

    project done

--- polishNotation.cpp
+++ polishNotation.cpp
@@ -40,7 +40,6 @@ void Stack<T>::push(T Data)   {
     
     Stack* temp = new Stack();
     if (isEmpty())  {
-        cout << "\nFirst element in the stack\n";
         temp->link = NULL;
         temp->data = Data;
         TOS = temp;
@@ -86,7 +85,7 @@ bool isOperand(char value)  {
     else return 0;
 }
 
-bool operatorPrecedence(char operatorRead, Stack<char>* TOS)  {
+bool operatorPrecedencePostfix(char operatorRead, Stack<char>* TOS)  {
     
     if (TOS == NULL)    return 0;
     if ((TOS->getData() == '+' && operatorRead == '-') || (TOS->getData() == '-' && operatorRead == '+'))  return 1;    // condition for pop
@@ -95,13 +94,22 @@ bool operatorPrecedence(char operatorRead, Stack<char>* TOS)  {
     return 0;
 }
 
-string infixToPostfix(string expression)   {
+bool operatorPrecedencePrefix(char operatorRead, Stack<char>* TOS)  {
+    
+    if (TOS == NULL)    return 0;
+    if ((TOS->getData() == '+' && operatorRead == '-') || (TOS->getData() == '-' && operatorRead == '+'))  return 0;    // condition for pop
+    if ((TOS->getData() == '*' || TOS->getData() == '/') && (operatorRead == '-' || operatorRead == '+'))   return 1;
+
+    return 0;
+}
+
+string infixToPostfix(string expression, int* flag)   {
 
     Stack<char> stackForOperator;
     Stack<char> *temp;
     string postfix;
     postfix = "";
-
+    
     for (int i = 0; expression[i] != '\0'; i++)    {
         if (expression[i] == '(' || expression[i] == ')' || expression[i] == ' ')   continue;
 
@@ -113,10 +121,14 @@ string infixToPostfix(string expression)   {
             i--;
             postfix += " ";
         }
+        else if(!IsOperator(expression[i]) && !IsOperator(expression[i])) {
+            postfix = postfix + expression[i] + " ";
+            *flag = 0;
+        }
         else {
 
-            if (operatorPrecedence(expression[i], stackForOperator.getTOS()))   {
-                while (operatorPrecedence(expression[i], stackForOperator.getTOS()))    {
+            if (operatorPrecedencePostfix(expression[i], stackForOperator.getTOS()))   {
+                while (operatorPrecedencePostfix(expression[i], stackForOperator.getTOS()))    {
                     postfix = postfix + stackForOperator.tos() + " ";
                     stackForOperator.pop();
                 }
@@ -135,6 +147,56 @@ string infixToPostfix(string expression)   {
         stackForOperator.pop();
     }
     return postfix;
+}
+
+string infixToPrefix(string expression, int* flag)   {
+
+    Stack<char> stackForOperator;
+    Stack<char> *temp;
+    string prefix;
+    prefix = "";
+
+    reverse(expression.begin(), expression.end());
+
+    for (int i = 0; expression[i] != '\0'; i++)    {
+        if (expression[i] == '(' || expression[i] == ')' || expression[i] == ' ')   continue;
+
+        if (isOperand(expression[i])) {
+            while(isOperand(expression[i])) {
+                prefix = prefix + expression[i];
+                i++;
+            }
+            i--;
+            prefix += " ";
+        }
+        else if(!IsOperator(expression[i]) && !IsOperator(expression[i])) {
+            *flag = 0;
+            prefix = prefix + expression[i] + " ";
+        }
+        else {
+
+            if (operatorPrecedencePrefix(expression[i], stackForOperator.getTOS()))   {
+                while (operatorPrecedencePrefix(expression[i], stackForOperator.getTOS()))    {
+                    prefix = prefix + stackForOperator.tos() + " ";
+                    stackForOperator.pop();
+                }
+                stackForOperator.push(expression[i]); 
+            }
+            else    {
+                stackForOperator.push(expression[i]);
+
+            }
+
+        }
+    }
+    temp = stackForOperator.getTOS();
+    while (!stackForOperator.isEmpty()) {
+        prefix = prefix + stackForOperator.tos() + " ";
+        stackForOperator.pop();
+    }
+    reverse(prefix.begin(),prefix.end());
+
+    return prefix;
 }
 
 int operateExpression(int value1, int value2, char Operator)  {
@@ -161,7 +223,7 @@ int evaluatePostfix(string expression)   {
     int val1, val2;
     int result = 0;
 
-    for(int i = 0; i < expression[i] != '\0'; i++)  {
+    for(int i = 0; expression[i] != '\0'; i++)  {
 
         if (expression[i] == ',' || expression[i] == ' ')   
             continue;
@@ -187,21 +249,128 @@ int evaluatePostfix(string expression)   {
     return result;
 }
 
+int evaluatePrefix(string expression)   {
+
+    Stack<int> prefix;
+    int val1, val2;
+    int result = 0;
+
+    for(int i = expression.length() - 1 ; i >= 0 ; i--)  {
+
+        if (expression[i] == ',' || expression[i] == ' ')   
+            continue;
+
+        if (IsOperator(expression[i]))  {
+            val1 = prefix.tos();
+            prefix.pop();
+            val2 = prefix.tos();
+            prefix.pop();
+            result = operateExpression(val1, val2, expression[i]);
+            prefix.push(result);
+        }
+        else {
+            int operand = 0;
+            while(isOperand(expression[i])) {
+                operand = (operand*10) + (expression[i] - 48);
+                i--;
+            }
+            i++;
+            prefix.push(operand);
+        }
+    }
+    return result;
+}
+
+void printRules()   {
+    cout << "\nRules for writing the expression: \n";
+    cout << "Input expression must be in a desired format. "  << endl
+         << "Operands must be integers and there should be space in between two operands" << endl
+         << "Only '+'  ,  '-'  , '*' and '/'  operators are expected" << endl;
+}
+
 int main()  {
 
     string expression;
     string result;
-    int res;
-    cout << "\nEnter an expression: ";
-    getline(cin, expression);
+    int res = 1, choice;
+    int *flag;
+    flag = &res;
 
-    result = infixToPostfix(expression);
-    cout << endl << result;
+    while(1)    {
 
-    res = evaluatePostfix(result);
-    cout << endl << res;
+    system("cls");
+    cout << "\n--IMPLEMENTATION OF POLISH NOTATION USING STACK--\n";
+    cout << "Available operations: \n";
+    cout << "1. Infix to postfix" << endl   
+         << "2. Infix to prefix" << endl
+         << "3. Evaluate a prefix expression" << endl
+         << "4. Evaluate a postfix expression" << endl
+         << "5. Evaluate an infix expression" << endl 
+         << "6. Exit" << endl
+         << "Your choice: ";
+    
+    cin >> choice ;
+    switch(choice)  {
+        case 1: {
+            cout << "\nEnter an expression: " ;
+            getline(cin >> ws,expression);
+            result = infixToPostfix(expression, flag);
+            cout << "\nPostfix expression: " << result;
+            if (*flag != 0) {
+                cout << "\nDo you wish to evaluate this expression(1/0): ";
+                cin >> choice;
+                if (choice == 1)    {
+                    cout << "\nResult after evaluation: " << evaluatePostfix(result);    
+                }
+            }
+            break;
+        }
+        case 2: {
+            cout << "\nEnter an expression: " ;
+            getline(cin >> ws,expression);
+            result = infixToPrefix(expression, flag);
+            cout << "\nPrefix expression: " << result;
+            if (*flag != 0) {
+                cout << "\nDo you wish to evaluate this expression(1/0): ";
+                cin >> choice;
+                if (choice == 1)    {
+                    cout << "\nResult after evaluation: " << evaluatePrefix(result);    
+                }
+            }
+            break;
+        }
+        case 3: {
+            printRules();
+            cout << "\nEnter your prefix expression: " ;
+            getline(cin >> ws,expression);
+            cout << "\nResult after evaluation: " << evaluatePrefix(expression);
+            break;
+        }
+        case 4: {
+            printRules();
+            cout << "\nEnter your postfix expression: " ;
+            getline(cin >> ws,expression);
+            cout << "\nResult after evaluation: " << evaluatePostfix(expression);
+            break;
+        }
+        case 5: {
+            cout << "\nEnter an expression: " ;
+            getline(cin >> ws,expression);
+            result = infixToPostfix(expression, flag); 
+            cout << "\nResult after evaluation: " << evaluatePostfix(result);
+            break;
+        }
+        case 6: {
+            exit(0);
+        }
+        default:    {
+            cout << "\nError: Invalid choice intered";
+            break;
+        }
+    }
 
     getchar();
+    }
 
     return 0;
 
